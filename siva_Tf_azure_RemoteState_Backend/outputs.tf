4e54a574c0fc6c94a5a2f6eded6bc1f833707338
SivaKumar2606 <93194209+SivaKumar2606@users.noreply.github.com>
2021-12-07 20:31:01 +0530

    Create outputs.tf

---
+++ siva_Tf_azure_RemoteState_Backend/outputs.tf
@@ -0,0 +1,25 @@
+
+# 1. Output Values - Virtual Machine - IP Address
+output "vm_public_ip_address" {
+  description = "My Virtual Machine Public IP"
+  value = azurerm_public_ip.myterraformpublicip.id
+}
+
+output "vm_name" {
+    description = "Name of the VM"
+    value = azurerm_linux_virtual_machine.myterraformvm.name
+}
+
+# 2. Output Values - Virtual Machine Resource Group Name
+output "vm_resource_group_name" {
+  description = "My Virtual Machine Resource Group Name"
+  value = azurerm_resource_group.myterraformgroup.name
+
+}
+
+# 3. Output Values - Virtual Machine Location
+output "vm_resource_group_location" {
+  description = "My Virtual Machine Location"
+  value = azurerm_resource_group.myterraformgroup.location
+}
+
