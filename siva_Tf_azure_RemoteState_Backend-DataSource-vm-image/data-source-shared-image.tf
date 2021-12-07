8be6992fa24ea3fcfc915975de90996f7d3ae5c4
SivaKumar2606 <93194209+SivaKumar2606@users.noreply.github.com>
2021-12-07 20:27:52 +0530

    Delete siva_Tf_azure_RemoteState_Backend-DataSource-vm-image directory

--- siva_Tf_azure_RemoteState_Backend-DataSource-vm-image/data-source-shared-image.tf
+++
@@ -1,6 +0,0 @@
-# Using the exsiting infra
-
-data "azurerm_image" "existing" {
-  name                = "worker-machine-Terraform-azcli-image-20211206204327"
-  resource_group_name = "TerraformRG"
-}
