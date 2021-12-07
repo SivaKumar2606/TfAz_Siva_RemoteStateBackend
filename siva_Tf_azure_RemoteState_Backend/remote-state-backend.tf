a56a1926cee10d880ae7b5a17254c1e75604c651
SivaKumar2606 <93194209+SivaKumar2606@users.noreply.github.com>
2021-12-07 17:45:20 +0530

    Add files via upload

---
+++ siva_Tf_azure_RemoteState_Backend/remote-state-backend.tf
@@ -0,0 +1,42 @@
+
+# Generate random text for a unique storage account name
+resource "random_id" "randomId" {
+    keepers = {
+        # Generate a new ID only when a new resource group is defined
+        resource_group = azurerm_resource_group.myterraformgroup.name
+    }
+
+    byte_length = 8
+}
+
+# Create storage account for boot diagnostics
+resource "azurerm_storage_account" "mystorageaccount" {
+    name                        = "diag${random_id.randomId.hex}"
+    resource_group_name         = azurerm_resource_group.myterraformgroup.name
+    location                    = azurerm_resource_group.myterraformgroup.location
+    account_tier                = "Standard"
+    account_replication_type    = "LRS"
+
+    tags = {
+        environment = "${var.env}"
+    }
+}
+
+# Create stroage containerblob for sampledata
+# Resource_group is not Called here. Please check Syntax
+resource "azurerm_storage_container" "mystoragecontainer" {
+    name                        = "${var.container_name}"
+    storage_account_name        = azurerm_storage_account.mystorageaccount.name
+    container_access_type       = "private"
+}
+
+# Redirecting .tfstate file to be saved on Existing Remote storage Backend Container account.
+# Only Values are Accepted here. No Variable Reference Calls
+terraform {
+ backend "azurerm" {
+  resource_group_name    = "TerraformRG"
+  storage_account_name   = "remotestatefilestoacc01"
+  container_name         = "tfbackendaccount"
+  key                    = "terraform.tfstate"
+ }
+}
